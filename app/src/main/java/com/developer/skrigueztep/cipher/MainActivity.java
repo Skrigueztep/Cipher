9a445d070edfc01f66d889c5a3d9e765f29574c4
Skrigueztep <skrigueztep@gmail.com>
2019-08-04 16:34:06 -0500

    Basic application complete

--- app/src/main/java/com/developer/skrigueztep/cipher/MainActivity.java
+++ app/src/main/java/com/developer/skrigueztep/cipher/MainActivity.java
@@ -20,10 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
     String TAG = "MainActivity: ";
 
-    char[] alphabet = {
-            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'
-    };
-
     int ROT = 13;
 
     @Override
@@ -45,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 if(text.isEmpty()){
                     showMessage(TAG, "Text field is empty");
                 }else{
-                    // String cipherTxt = cipher(text,ROT);
-                    txtCiphered.setText("Texto cifrado");
+                    String cipherTxt = cesarCipher(text,ROT);
+                    txtCiphered.setText(cipherTxt);
                     txtToCipher.setText("");
                     showMessage(TAG, "Text chipered");
                 }
@@ -61,24 +57,39 @@ public class MainActivity extends AppCompatActivity {
 
     /*
         Este metodo debe devolver una nueva cadena con el texto cifrado aplicando
-        la base el cifrado cesar utilizando el ROT estandar
-    */
+        la base del cifrado cesar utilizando el ROT estandar
 
-    private String cipher(String text, int rot_index){
-        for(int i = 0; i < text.length(); i++){     // Recorre el string
-            char letra = text.charAt(i);            // Encapsula la letra dada por el indice
-            for(int index = 0; index < alphabet.length; ){   // Recorre el char[]
-                if(letra == alphabet[index]){   //  Verifica que la letra coincida con el elemento dado por el indice en el alfabeto
-                    // Si son iguales suma el indice mas el rot y obtiene el elemento en la posicion
-                    // Y si es mayor el nuevo indice a 26 se resta (nuevo_index - 26 = resultado) en donde
-                    // El resultado va a ser el nuevo indice y se obtiene el elemento de ese nuevo indice
-                }else{
-                    // Si no son iguales se sigue recorriendo hasta que lo sea
-                    index++;
-                }
+        Reference: http://puntocomnoesunlenguaje.blogspot.com/2016/02/cifrado-cesar-java.html
+    */
+   /*
+
+    a=n     n=a
+    b=o     o=b
+    c=p     p=c
+    d=q     q=d
+    e=r     r=e
+    f=s     s=f
+    g=t     t=g
+    h=u     u=h
+    i=v     v=i
+    j=w     w=j
+    k=x     x=k
+    l=y     y=l
+    m=y     z=m
+
+   */
+
+    private String cesarCipher(String text, int rot_index){
+
+        StringBuilder cifrado = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if ((text.charAt(i) + rot_index) > 'z') {
+                cifrado.append((char) (text.charAt(i) + rot_index - 26));
+            } else {
+                cifrado.append((char) (text.charAt(i) + rot_index));
             }
         }
 
-        // Regresa un String con el texto generado por los indices
+        return cifrado.toString();
     }
 }
